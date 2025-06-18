1c65038cd77625b7b1ab004d5f6c18876feb953e
edvieria <evvasconcelos@hotmail.com>
2025-06-18 09:47:34 +0100

    ex00 done

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -2,4 +2,38 @@
 // Created by edvieira on 6/16/25.
 //
 
-#include "Fixed.h"
+#include "Fixed.hpp"
+
+
+int Fixed::getRawBits() const {
+    std::cout << "getRawBits() member function called" << std::endl;
+    return fixedPoint;
+}
+
+void Fixed::setRawBits(int const raw) const {
+    (void) raw;
+}
+
+
+// ======== constructor and destructor
+
+Fixed::Fixed() {
+    std::cout << "Default constructor called" << std::endl;
+    this->fixedPoint = 0;
+}
+
+Fixed::Fixed(Fixed const &other) {
+    std::cout << "Copy constructor operator called" << std::endl;
+    this->fixedPoint = other.getRawBits();
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(Fixed const &other) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->fixedPoint = other.getRawBits();
+    return *this;
+}
+
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -2,13 +2,28 @@
 // Created by edvieira on 6/16/25.
 //
 
-#ifndef FIXED_H
-# define FIXED_H
+#ifndef FIXED_HPP
+# define FIXED_HPP
 
+# include <iostream>
 
 
 class Fixed {
+private:
+    static const int fractBits = 8;
 
+    int fixedPoint;
+
+public:
+    int getRawBits(void) const;
+    void setRawBits(int const raw) const;
+
+    Fixed();
+    Fixed(Fixed const &other);
+
+    ~Fixed();
+
+    Fixed &operator=(Fixed const &other);
 };
 
 #endif //FIXED_H
