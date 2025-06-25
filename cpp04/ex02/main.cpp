591e8fa94b1e4c00da201db6d3f9f2d22126677a
Eduardo Vieira Vasconcelos <edvieira@c2r5s4.42porto.com>
2025-06-25 17:57:28 +0100

    finished ex02

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -17,8 +17,8 @@
 
 int	main(void)
 {
+	//AAnimal a= new AAnimal("sadads"); // will give compilation error, as expected
 	std::cout << "\n------------- Constructing -------------" << std::endl;
-
 
 	Cat *a1 = new Cat();
 	Cat *a2 = new Cat(*a1);
